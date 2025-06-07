924986f517a477b71c217ce04e13fbe0ed48f4c8
St Manía <piterfreik@gmail.com>
2025-06-06 19:57:52 -0500

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -7,7 +7,7 @@ let pesan = args.join` `
 let oi = `📄 *MENSAJE:* ${pesan}`
 let teks = `╭━〔 *Te Habla Barbie-Bot DESPIERTEN* 〕━⬣\n${oi}\n\n`
 for (let mem of participants) {
-teks += `┃🤖⊹ @${mem.id.split('@')[0]}\n`}
+teks += `┃🌸⊹ @${mem.id.split('@')[0]}\n`}
 teks += `┃
 ╰━━━━━[ Barbie Bot ]━━━━━⬣`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
